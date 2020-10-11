7c53d2319742314ee1086d5b968c80bb1bce8ccb
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-10-11 13:45:56 -0400

    Added Homescreen component

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import product from '../products'
+
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+        <Row>
+{product.map((products) => (
+  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+    <Product product={products} />
+  </Col>
+))}
+</Row>
+
+            </>
+
+    )
+}
+
+export default HomeScreen
