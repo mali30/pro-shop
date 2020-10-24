fc6a9dab73eccec15251ca96b9f3962f33e4dcdb
Mohamed Ali <mohamed.ali8434@gmail.com>
2020-10-24 17:35:29 -0400

    updated users file with name change

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcryptjs";
+
+// creating array of 3 users with one Admin
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    // hashing password synchronously for now
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "MO MO",
+    email: "momo@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+
+export default users;
