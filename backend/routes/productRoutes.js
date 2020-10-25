81a99600987070ffcbeccf5f36f6fdcda3ed2862
Mohamed Ali <mohamed.ali8434@gmail.com>
2020-10-25 13:40:53 -0400

    added routes for files into seperate file.

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+const router = express.Router()
+
+/*
+    adding asyncHandler package to handle async failures 
+    instead of having to try-catch blocks everywhere
+*/
+
+
+/*  @desc fetch all products
+    @route GET /api/products
+    @access public
+ */
+router.get("/", asyncHandler ( async (req, res) => {
+    // gets all products
+    const products = await Product.find({});
+
+    res.json(products);
+}))
+
+/* @desc fetch a single product
+   @route GET /api/products/:id
+   @access public
+ */
+
+router.get("/:id", asyncHandler (async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    console.log("Found products with id" + product)
+    // if we have a product with this id
+    if (product) {
+        res.json(product);
+    } else {
+        console.log("not found")
+        res.status(404).json({
+            message: "Product not found"
+        })
+    }
+}))
+
+
+
+export default router;
