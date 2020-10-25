cab9839813d73f678b826aeaa7c8c4918084f95a
Mohamed Ali <mohamed.ali8434@gmail.com>
2020-10-25 13:40:31 -0400

    refactored. moved products routes into it's own file also.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import colors from 'colors' 
-
-import products from './data/products.js'
+import colors from 'colors'
 import connectToMongo from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -16,21 +15,12 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-/*
-    get all products
-*/
-app.get("/api/products", (req, res) => {
-    res.json(products);
-})
+app.use('/api/products' , productRoutes)
 
-/*
-    get product for a based on id
-*/
-app.get("/api/products/:id", (req, res) => {
-    const productInURL = products.find((p) => req.params.id === p._id)
-    res.json(productInURL);
-})
-
-const PORT = process.env.PORT || 6000
-
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
+const PORT = process.env.PORT || 8080;
+if (!process.env.PORT) {
+    console.log(`Port is unavailalbe ${PORT}`)
+    process.exit(1);
+} else {
+    app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
+}
